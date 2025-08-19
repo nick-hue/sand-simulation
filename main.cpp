2c5aa83e7531c17a49e80ca5689de5e40b0f7ef0
nick-hue <nickagelid@gmail.com>
2025-08-19 21:06:24 +0300

    made grid work and hold cells with clicks

--- main.cpp
+++ main.cpp
@@ -1,51 +1,100 @@
 
 #include "raylib.h"
+#include <iostream>
 
-#define CELL_SIZE 20
+#define CELL_SIZE 15
+#define CELL_COLOR DARKGRAY
+#define LINE_COLOR WHITE
 
-void draw_grid()
-{
-    int screenWidth = GetScreenWidth();
-    int screenHeight = GetScreenHeight();
+#define SCREEN_WIDTH 450
+#define SCREEN_HEIGHT 900
+#define GRID_WIDTH SCREEN_WIDTH / CELL_SIZE
+#define GRID_HEIGHT SCREEN_HEIGHT / CELL_SIZE
 
-    for (int x = 0; x < screenWidth; x += CELL_SIZE)
-    {
-        DrawLine(x, 0, x, screenHeight, DARKGRAY);
-    }
+int grid_filled[GRID_WIDTH][GRID_HEIGHT] = {0};
 
-    for (int y = 0; y < screenHeight; y += CELL_SIZE)
-    {
-        DrawLine(0, y, screenWidth, y, DARKGRAY);
+void DrawGridOutline(){
+    // horizontal
+    for (int x = 0; x < SCREEN_WIDTH; x += CELL_SIZE)
+        DrawLine(x, 0, x, SCREEN_HEIGHT, LINE_COLOR);
+    
+    // vertical
+    for (int y = 0; y < SCREEN_HEIGHT; y += CELL_SIZE)
+        DrawLine(0, y, SCREEN_WIDTH, y, LINE_COLOR);
+
+}
+
+void FillCell(int x, int y, Color color){
+    DrawRectangle(x * CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE, color);
+}
+
+void DrawFilledCells(){
+    
+    for (int x = 0; x < GRID_WIDTH; x++){
+        for (int y = 0; y < GRID_HEIGHT; y++){
+            if (grid_filled[x][y] == 0){
+                FillCell(x, y, BLACK);
+            } else {
+                FillCell(x, y, WHITE);
+            }
+        }
     }
 }
 
+void DrawGrid()
+{
+    // Draw filled cells
+    DrawFilledCells();
+    // Draw grid outline
+    DrawGridOutline();
+
+
+}
+
+void DrawSelection(){
+    Vector2 mouse_pos = GetMousePosition();
+    if ((mouse_pos.x < 0 || mouse_pos.x > SCREEN_WIDTH) || (mouse_pos.y < 0 || mouse_pos.y > SCREEN_HEIGHT)){
+        printf("Clicking out of bounds...\n");
+        return;
+    }
+    // printf("x: %f - y: %f\n", mouse_pos.x, mouse_pos.y);
+
+    int cell_x = mouse_pos.x / CELL_SIZE;
+    int cell_y = mouse_pos.y / CELL_SIZE;
+
+    printf("CELL = %d - %d\n", cell_x, cell_y);
+    
+    // Show currently selected cell
+    FillCell(cell_x, cell_y, WHITE);
+
+    // Store clicked cell
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
+        grid_filled[cell_x][cell_y] = 1;
+    }
+
+    // printf("CELL x: %f - CELL y: %f\n", cell.x, cell.y);
+
+}
+
+// Copilot: Enable in Command Palette.
+
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    printf("GRID WIDTH : %d\n", GRID_WIDTH);
+    printf("GRID HEIGHT : %d\n", GRID_HEIGHT);
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sand Simulation");
+    SetWindowPosition(400, 100);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    SetTargetFPS(60);               
+    
+    while (!WindowShouldClose())
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
         BeginDrawing();
-
-            ClearBackground(BLACK);
-
-            draw_grid();
+        ClearBackground(BLACK);
+        DrawGrid();
+        DrawSelection();
             
-
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
